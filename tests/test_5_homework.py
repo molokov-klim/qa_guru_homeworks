5f7ba09fa0fd92169e5359528d125d12f5bed5b8
k.molokov <k.molokov@atol.ru>
2023-08-13 02:31:01 +0300

    homework_5

--- tests/test_5_homework.py
+++ tests/test_5_homework.py
@@ -1,15 +1,20 @@
-from datetime import time
+import inspect
+from datetime import time, datetime
 
 
 def test_dark_theme_by_time():
     """
     Протестируйте правильность переключения темной темы на сайте в зависимости от времени
+    (с 22 до 6 часов утра - ночь)
     """
-    current_time = time(hour=23)
-    # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
+    current_time = datetime.now()
 
-    is_dark_theme = None
-    assert is_dark_theme is True
+    is_dark_theme = False
+    if 6 < current_time.hour < 22:
+        is_dark_theme = True
+        assert is_dark_theme is True
+    else:
+        assert is_dark_theme is False
 
 
 def test_dark_theme_by_time_and_user_choice():
@@ -22,10 +27,15 @@ def test_dark_theme_by_time_and_user_choice():
     """
     current_time = time(hour=16)
     dark_theme_enabled_by_user = True
-    # TODO переключите темную тему в зависимости от времени суток,
-    #  но учтите что темная тема может быть включена вручную
 
-    is_dark_theme = None
+    if not dark_theme_enabled_by_user:
+        if 6 < current_time.hour < 22:
+            is_dark_theme = False
+        else:
+            is_dark_theme = True
+    else:
+        is_dark_theme = True
+
     assert is_dark_theme is True
 
 
@@ -41,12 +51,20 @@ def test_find_suitable_user():
         {"name": "Maria", "age": 18},
     ]
 
-    # TODO найдите пользователя с именем "Olga"
     suitable_users = None
+
+    for user in users:
+        if user.get('name') == 'Olga':
+            suitable_users = user
+
     assert suitable_users == {"name": "Olga", "age": 45}
 
-    # TODO найдите всех пользователей младше 20 лет
-    suitable_users = None
+    suitable_users = []
+
+    for user in users:
+        if int(user.get('age')) < 20:
+            suitable_users.append(user)
+
     assert suitable_users == [
         {"name": "Stanislav", "age": 15},
         {"name": "Maria", "age": 18},
@@ -63,6 +81,17 @@ def test_find_suitable_user():
 # >>> open_browser(browser_name="Chrome")
 # "Open Browser [Chrome]"
 
+def pretty_string(*args):
+    if len(args) > 1:
+        arg = "[" + ", ".join(str(a) for a in args) + "]"
+    else:
+        arg = "[" + str(*args) + "]"
+
+    method_name = str(inspect.stack()[1].function)
+    actual_result = f"{method_name.title()} {arg}".replace('_', " ")
+    print(actual_result)
+    return actual_result
+
 
 def test_readable_function():
     open_browser(browser_name="Chrome")
@@ -71,15 +100,15 @@ def test_readable_function():
 
 
 def open_browser(browser_name):
-    actual_result = None
+    actual_result = pretty_string(browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = pretty_string(page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = pretty_string(page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
