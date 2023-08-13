43207ac21c3ebe61a9f7af9e5a28c41398edefa9
k.molokov <k.molokov@atol.ru>
2023-08-13 03:10:27 +0300

    homework_5

--- tests/test_5_homework.py
+++ tests/test_5_homework.py
@@ -1,39 +1,46 @@
 # coding: utf-8
 import os
-from selene import be, have
+from selene import be, have, command
 from pages.page_automation_practice_form import PageAutomationPracticeForm
 
 
 def test_main_positive(browser_den):
-    page = PageAutomationPracticeForm(browser_den)
+    browser_den.open("https://demoqa.com/automation-practice-form")
 
-    page.first_name.should(be.blank).type('Klim')
-    page.last_name.should(be.blank).type('Molokov')
-    page.email.type('super@test.com')
-    page.gender_male.click()
-    page.phone.type('8123123123')
-    page.date_of_birth_modal_month.send_keys("March")
-    page.date_of_birth_modal_year.send_keys("1989")
-    page.date_of_birth_modal_day(day="05").click()
-    page.subjects.send_keys("some objects")
-    page.hobbie_reading.click()
-    page.hobbie_sports.click()
-    page.hobbie_music.click()
-    page.upload_file.send_keys(os.path.abspath('tests/test_data/1.bmp'))
-    page.current_address.type("some address 186")
-    page.state('Uttar Pradesh').click()
-    page.city('Agra').click()
-    page.current_address.submit()
+    browser_den.element('#firstName').perform(command.js.scroll_into_view).should(be.blank).type('Klim')
+    browser_den.element('#lastName').perform(command.js.scroll_into_view).should(be.blank).type('Molokov')
+    browser_den.element('#userEmail').perform(command.js.scroll_into_view).type('super@test.com')
+    browser_den.all('[name=gender]').element_by(have.value('Male')).element('..').perform(command.js.scroll_into_view).click()
+    browser_den.element('#userNumber').perform(command.js.scroll_into_view).type('8123123123')
 
-    page.final_table.should(have.exact_texts(
+    browser_den.element('#dateOfBirthInput').perform(command.js.scroll_into_view).click()
+    browser_den.element('.react-datepicker__month-select').perform(command.js.scroll_into_view).send_keys("March")
+    browser_den.element('#dateOfBirthInput').perform(command.js.scroll_into_view).click()
+    browser_den.element('.react-datepicker__year-select').send_keys("1989")
+    browser_den.element('#dateOfBirthInput').perform(command.js.scroll_into_view).click()
+    browser_den.element(f'.react-datepicker__day--005').click()
+    browser_den.element('#subjectsInput').perform(command.js.scroll_into_view).send_keys("some objects")
+    browser_den.all('#hobbiesWrapper .custom-control-label').element_by(have.exact_text('Sports')).perform(command.js.scroll_into_view).click()
+    browser_den.all('#hobbiesWrapper .custom-control-label').element_by(have.exact_text('Reading')).perform(command.js.scroll_into_view).click()
+    browser_den.all('#hobbiesWrapper .custom-control-label').element_by(have.exact_text('Music')).perform(command.js.scroll_into_view).click()
+    browser_den.element('#uploadPicture').perform(command.js.scroll_into_view).send_keys(os.path.abspath('tests/test_data/1.bmp'))
+    browser_den.element('#currentAddress').perform(command.js.scroll_into_view).type("some address 186")
+    browser_den.element('#state').click().perform(command.js.scroll_into_view)
+    browser_den.all('[id^=react-select][id*=option]').element_by(have.exact_text('Uttar Pradesh')).click()
+    browser_den.element('#city').click().perform(command.js.scroll_into_view)
+    browser_den.all('[id^=react-select][id*=option]').element_by(have.exact_text('Agra')).click()
+    browser_den.element('#currentAddress').perform(command.js.scroll_into_view).submit()
+
+    browser_den.all('.table-responsive .table td:nth-child(2)').should(have.exact_texts(
         'Klim Molokov',
         'super@test.com',
         'Male',
         '8123123123',
         '05 March,1989',
         '',
-        'Reading, Sports, Music',
+        'Sports, Reading, Music',
         '1.bmp',
         'some address 186',
         'Uttar Pradesh Agra'
     ))
+
