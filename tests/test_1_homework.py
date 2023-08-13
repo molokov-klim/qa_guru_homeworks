51383d94923c58d6ec56229643321304b6963317
k.molokov <k.molokov@atol.ru>
2023-08-13 19:57:59 +0300

    Удалил лишнее

--- tests/test_1_homework.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-from selene.support.shared import browser
-from selene import be, have
-
-
-def test_homework_1():
-    browser.open('https://google.com')
-    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
-    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests'))
