2e0b4b497bee7784970e04307620145d15801b15
k.molokov <k.molokov@atol.ru>
2023-08-13 20:06:39 +0300

    hotfix

--- tests/test_2_homework.py
+++ tests/test_2_homework.py
@@ -1,10 +1,11 @@
 # coding: utf-8
 from selene import be, have
+from selene import browser
 
 
-def test_homework_2(browser_density):
-    browser_density.open('https://google.com')
+def test_homework_2():
+    browser.open('https://google.com')
     random_string = "uyfihfhgfxcjkjhkljljkhjfgfchghvkjggfhhglkgfgdjvhjklllkl,lkjhjghfgcjh"
-    browser_density.element('[name="q"]').should(be.blank).type(random_string).press_enter()
-    browser_density.element('//*[@id="botstuff"]/div/div[2]/div/p[1]').should(
+    browser.element('[name="q"]').should(be.blank).type(random_string).press_enter()
+    browser.element('//*[@id="botstuff"]/div/div[2]/div/p[1]').should(
         have.text(f'По запросу {random_string} ничего не найдено.'))
