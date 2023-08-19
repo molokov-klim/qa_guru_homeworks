108bf96ee8dcb300b5eb5f238b11abc561b04015
k.molokov <k.molokov@atol.ru>
2023-08-19 16:16:14 +0300

    homework_8

--- homework/test_shop.py
+++ homework/test_shop.py
@@ -1,5 +1,5 @@
 """
-Протестируйте классы из модуля homework/models.py
+Тестовый модуль для проверки классов из homework.models
 """
 
 import pytest
@@ -26,8 +26,7 @@ def cart(request) -> Cart:
 
 class TestProducts:
     """
-    Тестовый класс - это способ группировки ваших тестов по какой-то тематике
-    Например, текущий класс группирует тесты на класс Product
+    Тесты на класс Product
     """
     check_quantity_positive_modifiers = (0, -1, -50)
     check_quantity_negative_modifiers = (1, 50)
@@ -64,26 +63,13 @@ class TestProducts:
 
 class TestCart:
     """
-    TODO Напишите тесты на методы класса Cart
-        На каждый метод у вас должен получиться отдельный тест
-        На некоторые методы у вас может быть несколько тестов.
-        Например, негативные тесты, ожидающие ошибку (используйте pytest.raises, чтобы проверить это)
+    Тесты на класс Cart
     """
     add_product_positive_modifiers = (1, 100, 99999)
     add_product_negative_modifiers = (0, -1, -100, -99999)
 
     @pytest.mark.parametrize("modifier", add_product_positive_modifiers)
     def test_add_product_positive(self, cart, products, modifier):
-        """
-        if buy_count < 0:
-            raise ValueError("Некорректное количество товара")
-        if product.__hash__() in self.products:
-            self.products[product] += buy_count
-            return cast('Cart', self)
-        self.products[product] = buy_count
-        return cast('Cart', self)
-        :return:
-        """
         item = products[0]
         cart.add_product(product=item, buy_count=modifier)
         before_quantity = cart.products[item]
@@ -93,16 +79,6 @@ class TestCart:
 
     @pytest.mark.parametrize("modifier", add_product_negative_modifiers)
     def test_add_product_negative(self, cart, products, modifier):
-        """
-        if buy_count < 0:
-            raise ValueError("Некорректное количество товара")
-        if product.__hash__() in self.products:
-            self.products[product] += buy_count
-            return cast('Cart', self)
-        self.products[product] = buy_count
-        return cast('Cart', self)
-        :return:
-        """
         item = products[0]
         with pytest.raises(ValueError):
             cart.add_product(product=item, buy_count=modifier)
@@ -110,15 +86,6 @@ class TestCart:
             cart.add_product(product=item, buy_count=modifier)
 
     def test_remove_product_negative(self, cart, products):
-        """
-        if product not in self.products:
-            return cast('Cart', self)
-        if remove_count is None or remove_count >= self.products[product]:
-            del self.products[product]
-        else:
-            self.products[product] -= remove_count
-        return cast('Cart', self)
-        """
         item = products[0]
         cart.add_product(product=item, buy_count=1)
         cart.remove_product(product=item, remove_count=1)
@@ -126,10 +93,6 @@ class TestCart:
             assert cart.products[item] == 0
 
     def test_clear(self, cart, products):
-        """
-        self.products.clear()
-        return cast('Cart', self)
-        """
         item = products[0]
         cart.add_product(product=item, buy_count=1)
         cart.clear()
@@ -137,31 +100,27 @@ class TestCart:
             assert cart.products[item] == 0
 
     def test_get_total_price(self, cart, products):
-        """
-        amount = 0
-        for product, quantity in self.products.items():
-            amount += product.price * quantity
-        return amount
-        """
-        ...
+        item = products[0]
+        cart.add_product(product=item, buy_count=3)
+        assert cart.get_total_price() == 300.0
 
-    def test_buy(self, cart, products):
-        """
-        for product, quantity in self.products.items():
-            if not product.check_quantity(quantity):
-                raise ValueError(f"Товара {product} недостаточно на складе. Остаток: {quantity}")
-        assert self.process_payment()
-        for product, quantity in self.products.items():
-            product.buy(quantity)
-        self.clear()
-        return cast('Cart', self)
-        """
-        ...
+    def test_buy_positive(self, cart, products):
+        item = products[0]
+        quantity = item.quantity
+        cart.add_product(product=item, buy_count=quantity)
+        cart.buy()
+        assert cart.get_total_price() == 0
+        item.quantity = quantity
+
+    def test_buy_negative(self, cart, products):
+        item = products[0]
+        quantity = item.quantity + 1
+        cart.add_product(product=item, buy_count=quantity)
+        with pytest.raises(ValueError):
+            cart.buy()
 
     def test_process_payment(self, cart, products):
-        """
-        amount = self.get_total_price()
-        # payment mock
-        return True
-        """
-        ...
+        item = products[0]
+        quantity = item.quantity
+        cart.add_product(product=item, buy_count=quantity)
+        assert cart.process_payment() is True
